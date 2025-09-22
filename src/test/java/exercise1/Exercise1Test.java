c760e690183442b51b43e4f421cba469d2dad69d
leilaweicman <leilaweic@hotmail.com>
2025-09-22 13:50:00 +0200

    test(exercise1): add assertions to verify equality and inequality of integers using AssertJ

---
+++ src/test/java/exercise1/Exercise1Test.java
@@ -0,0 +1,22 @@
+package exercise1;
+
+import org.testng.annotations.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class Exercise1Test {
+
+    @Test
+    void integersShouldBeEqual() {
+        Integer a = 10;
+        Integer b = 10;
+        assertThat(a).isEqualTo(b);
+    }
+
+    @Test
+    void integersShouldBeNotEqual() {
+        Integer a = 5;
+        Integer b = 10;
+        assertThat(a).isNotEqualTo(b);
+    }
+}
