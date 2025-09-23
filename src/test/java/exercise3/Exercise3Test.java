8a57cafecb2563f5065d9acc6e04a81c8483f053
leilaweicman <leilaweic@hotmail.com>
2025-09-23 10:11:01 +0200

    test(exercise2): add assertion to verify arrays are identical using AssertJ

---
+++ src/test/java/exercise3/Exercise3Test.java
@@ -0,0 +1,16 @@
+package exercise3;
+
+import org.testng.annotations.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Exercise3Test {
+
+    @Test
+    void integerArrayShouldBeEqual() {
+        int[] array1 = {1, 2, 3, 4, 5};
+        int[] array2 = {1, 2, 3, 4, 5};
+
+        assertThat(array1).isEqualTo(array2);
+    }
+}
